a4a88bdb1af9264466ccead9fb5918dd5498241c
Maary <24504742+Steve-Mr@users.noreply.github.com>
2023-03-01 22:20:16 +0800

    loop_goals_time now set orientation for goal and marker

--- navigation/src/loop_goals_time.cpp
+++ navigation/src/loop_goals_time.cpp
@@ -3,7 +3,7 @@
 #include <actionlib/client/simple_action_client.h>
 #include <iostream>
 
-#include<unistd.h>               // for linux 
+#include <unistd.h>               // for linux 
 
 #include <boost/shared_ptr.hpp>
 #include <std_msgs/String.h>
@@ -16,6 +16,10 @@
 #include <visualization_msgs/Marker.h>
 
 #include <list>
+
+#include <cmath>
+#include <tf/transform_datatypes.h>
+#include <geometry_msgs/Quaternion.h>
 
 using namespace std;
 
@@ -41,6 +45,7 @@ int main(int argc, char** argv){
     goal.target_pose.header.stamp = ros::Time::now();
     
     boost::shared_ptr<geometry_msgs::PointStamped const> waypoint;
+	boost::shared_ptr<geometry_msgs::PointStamped const> prev_waypoint;
     std::list<boost::shared_ptr<geometry_msgs::PointStamped const>> points_list;
     
     // 在 visualization_marker 上广播
@@ -88,6 +93,17 @@ int main(int argc, char** argv){
 
 		// 将标记点添加到列表中
 		points_list.push_back(waypoint);
+
+		// double theta = 0.0;
+		// if (points_list.size() != 1)
+		// 	theta = atan2(waypoint->point.y - prev_waypoint->point.y, waypoint->point.x - prev_waypoint->point.x);
+		// else
+		// {
+		// 	theta = atan2(waypoint->point.y - 0.0, waypoint->point.x - 0.0);
+		// }
+		// prev_waypoint = waypoint;
+		// geometry_msgs::Quaternion q = tf::createQuaternionMsgFromYaw(theta);
+		// points_marker.pose.orientation = q;
     	
     	// 生成并发布 Marker 在 rviz 中显示
     	geometry_msgs::Point p;
@@ -112,15 +128,64 @@ int main(int argc, char** argv){
     	// if(points_list.size() != 1){
     	// 	points_list.push_back(points_list.front());
     	// }
-    	for(auto const& point: points_list){
+		auto prev_iter = points_list.begin();
+    	for(auto it = points_list.begin(); it != points_list.end(); ++it){
+			auto const& point = *it;
+			auto const& prev = *prev_iter;
+			
     		ROS_INFO("goal is %f", point->point.x);
     		ROS_INFO("goal is %f", point->point.y);
     		
-    	    goal.target_pose.pose.position.x = point->point.x;
+			double theta = 0;
+
+			if (it != points_list.begin()){
+				theta = atan2(point->point.y - prev->point.y, point->point.x - prev->point.x);
+				ROS_INFO("prev.x is %f", prev->point.x);
+				ROS_INFO("prev.y is %f", prev->point.y);}
+			else
+			{
+				theta = atan2(point->point.y - 0.0, point->point.x - 0.0);
+			}
+			
+			prev_iter = it;
+			geometry_msgs::Quaternion q = tf::createQuaternionMsgFromYaw(theta);
+
+			goal.target_pose.pose.position.x = point->point.x;
     		goal.target_pose.pose.position.y = point->point.y;
-   			goal.target_pose.pose.orientation.w = 1.0;
-   			
-   			cout << "\nGoing to next point." << endl;
+			goal.target_pose.pose.orientation = q;
+
+			visualization_msgs::Marker arrow_marker;
+			arrow_marker.header.frame_id = "map";
+			arrow_marker.header.stamp = ros::Time();
+
+			// 设置 Marker 的 namespace 和 id。
+			// 相同的 namespace 和 id 相同时新 marker 会覆盖旧值
+			arrow_marker.ns = "points";
+			arrow_marker.id = 0;
+
+			// 动作：支持添加 ADD/删除 DELETE/删除全部 DELETEALL
+			arrow_marker.action = visualization_msgs::Marker::ADD;
+			arrow_marker.pose.orientation.w = 1.0;
+
+			arrow_marker.type = visualization_msgs::Marker::ARROW;
+
+			// Marker 的比例，这里 1.0 为 1m
+			arrow_marker.scale.x = 1.0;
+			arrow_marker.scale.y = 0.2;
+			arrow_marker.scale.z = 0.0;
+
+			// 设置 Marker 颜色（此处为绿色
+			arrow_marker.color.g = 1.0f;
+			arrow_marker.color.a = 1.0;
+
+			arrow_marker.pose.position.x = point->point.x;
+			arrow_marker.pose.position.y = point->point.y;
+			arrow_marker.pose.position.z = 0;
+			arrow_marker.pose.orientation = q;
+
+			marker_pub.publish(arrow_marker);
+
+			cout << "\nGoing to next point." << endl;
     
     		// 发送目标使小车开始移动，超时时间 180s
     		ac.sendGoalAndWait(goal, ros::Duration(180.0,0), ros::Duration(180.0,0));
